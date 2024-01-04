6eaa307960bf1bdda21862685f2b3e2ebb76c6b5
Tinatin <tinatinmghebrishvili@gmail.com>
2024-01-04 04:14:22 +0400

    applying prettier

--- src/pages/ProductsPages/DetailedProductPage.jsx
+++ src/pages/ProductsPages/DetailedProductPage.jsx
@@ -2,8 +2,13 @@ import Slider from "../../components/Slider";
 import PhotoSwiper from "../../components/PhotoSwiper";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
-import { faHeart, faChevronRight, faChevronLeft, faCircle } from "@fortawesome/free-solid-svg-icons";
-import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
+import {
+  faHeart,
+  faChevronRight,
+  faChevronLeft,
+  faCircle,
+} from "@fortawesome/free-solid-svg-icons";
+import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useProductsContext } from "../../contexts/ProductsContextProvider";
 import { DetailedProducts, SimilarProductTitle } from "../AllPages";
@@ -15,34 +20,37 @@ const DetailedProductPage = () => {
   const { productId } = useParams();
   const { mainPhoto, addFav: addtoFav, clothes } = useProductsContext();
   const imagesForSlider = clothes.filter((product) => product.id !== productId);
-  const [details, setDetails] = useState(false)
-  const navigate = useNavigate()
+  const [details, setDetails] = useState(false);
+  const navigate = useNavigate();
 
-  const isMobileView = window.innerWidth <= 1300
+  const isMobileView = window.innerWidth <= 1300;
   return (
     <div>
       {clothes?.map(
         (prod) =>
           prod.id === productId && (
             <DetailedProducts key={prod.id}>
-              {isMobileView && <FontAwesomeIcon
+              {isMobileView && (
+                <FontAwesomeIcon
                   className="back-button"
                   onClick={() => navigate(-1)}
                   icon={faChevronLeft}
                   size="lg"
-                />}
-                {isMobileView && 
-                  <SliderForMobile images={[prod.image, ...prod.moreImages]}/>
-                 }  
+                />
+              )}
+              {isMobileView && (
+                <SliderForMobile images={[prod.image, ...prod.moreImages]} />
+              )}
               <div className="detailed-slider">
                 <PhotoSwiper photos={prod.moreImages} id={prod.id} />
               </div>
-              {!isMobileView && 
-              <img
-                className="detailed-product-image"
-                src={mainPhoto || prod.image}
-                alt=""
-              />}
+              {!isMobileView && (
+                <img
+                  className="detailed-product-image"
+                  src={mainPhoto || prod.image}
+                  alt=""
+                />
+              )}
               <div className="detailed-product-details">
                 <div className="detailed-name-price">
                   <h1 className="detailed-product-name">{prod.name}</h1>
@@ -50,12 +58,11 @@ const DetailedProductPage = () => {
                   <h2 className="detailed-product-price">{prod.price}₾</h2>
                 </div>
                 <div>
-                <FontAwesomeIcon 
-                  icon={faCircle}
-                  style={{color: `${prod.color}`}}
-                  className="color-icon"
-                />
-
+                  <FontAwesomeIcon
+                    icon={faCircle}
+                    style={{ color: `${prod.color}` }}
+                    className="color-icon"
+                  />
                 </div>
                 <div onClick={() => addtoFav(prod)} className="d-p-favorite">
                   {}
@@ -66,14 +73,26 @@ const DetailedProductPage = () => {
                   </h2>
                   <FontAwesomeIcon
                     className="heart-icon"
-                    icon={(isMobileView   && !prod.isFavorited) ? farHeart: faHeart}
-                    style={{ color: prod.isFavorited ? "brown" : "black"}}
+                    icon={
+                      isMobileView && !prod.isFavorited ? farHeart : faHeart
+                    }
+                    style={{ color: prod.isFavorited ? "brown" : "black" }}
                   />
                 </div>
-                <h5 className="product-description">{t("Description")}
-                {isMobileView && <FontAwesomeIcon onClick={() => setDetails(prevState => !prevState)} icon={faChevronRight} />}
+                <h5 className="product-description">
+                  {t("Description")}
+                  {isMobileView && (
+                    <FontAwesomeIcon
+                      onClick={() => setDetails((prevState) => !prevState)}
+                      icon={faChevronRight}
+                    />
+                  )}
                 </h5>
-                {(!isMobileView || details) && <h5 className="product-description-text">{prod.description}</h5>}
+                {(!isMobileView || details) && (
+                  <h5 className="product-description-text">
+                    {prod.description}
+                  </h5>
+                )}
               </div>
             </DetailedProducts>
           )
